21bc0743bde41df36b7d7389aedd5c2c104d0973
tellll <sketteruru@gmail.com>
2017-09-04 17:49:14 +0900

    微更新

--- flasker/python_scraping2/pydocTest.jl
+++ flasker/python_scraping2/pydocTest.jl
@@ -1,3 +1,5 @@
 {"title": "formatter", "link": "library/formatter.html?highlight=for#module-formatter"}
 {"link": "library/formatter.html?highlight=for#module-formatter", "title": "formatter"}
 {"link": "library/formatter.html?highlight=for#module-formatter", "title": "formatter"}
+{"title": "formatter", "link": "library/formatter.html?highlight=for#module-formatter"}
+{"link": "library/formatter.html?highlight=for#module-formatter", "title": "formatter"}
